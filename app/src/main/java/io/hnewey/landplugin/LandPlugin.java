de15640c1893540cba27908ae1df76acb22e2d57
Harry New <hnewey7@gmail.com>
2026-09-12 08:06:58 +0100

    Initial commit

---
+++ app/src/main/java/io/hnewey/landplugin/LandPlugin.java
@@ -0,0 +1,21 @@
+package io.hnewey.landplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.kyori.adventure.text.Component;
+
+public class LandPlugin extends JavaPlugin implements Listener {
+    @Override 
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler 
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text("Hello!"));
+    }
+}
